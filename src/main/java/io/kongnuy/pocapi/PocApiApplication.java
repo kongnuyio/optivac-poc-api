2fd42082dfe3f042a20a83a531afcd70dee4ce89
kongnuy <kongnuy@avomark.fr>
2024-04-01 10:17:37 +0200

    feat: Initial Dev commit

---
+++ src/main/java/io/kongnuy/pocapi/PocApiApplication.java
@@ -0,0 +1,13 @@
+package io.kongnuy.pocapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/kongnuy/pocapi/PocApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.kongnuy.pocapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
